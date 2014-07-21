657ef5196932332cc4ace1337e1fa844ff737d58
ouchliu <chaotengliu@gmail.com>
2014-07-20 20:42:27 -0700

    chapter 1

---
+++ arraysAndStrings/arraysAndStrings/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  arraysAndStrings
+//
+//  Created by Charlie on 7/20/14.
+//  Copyright (c) 2014 Charlie. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
