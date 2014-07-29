bc4ff5337fe4e679a9747afe0b6017f20277f387
ouchliu <chaotengliu@gmail.com>
2014-07-28 23:50:57 -0700

    first question

--- arraysAndStrings/arraysAndStrings/main.m
+++ arraysAndStrings/arraysAndStrings/main.m
@@ -7,16 +7,44 @@
 //
 
 #import <Foundation/Foundation.h>
-
+int max(int a, int b)
+{
+    return a > b?a:b;
+}
 int main(int argc, const char * argv[])
 {
 
     @autoreleasepool {
+        NSMutableDictionary *glossary = [NSMutableDictionary dictionary];
+        glossary[@"a"] = @"aa";
+        glossary[@"b"] = @"bb";
+        glossary[@"c"] = @"cc";
         
-        // insert code here...
-        NSLog(@"Hello, World!");
+        NSLog(@"%@", glossary[@"a"]);
+        NSLog(@"%@", glossary[@"b"]);
+        NSLog(@"%@", glossary[@"c"]);
+        
+        NSDictionary *glossary2 = [NSDictionary dictionaryWithObjectsAndKeys:
+            @"bb", @"b",
+                                   @"aa", @"a",
+            nil
+         ];
+        
+        for (NSString *key in glossary2) {
+            NSLog(@"%@", glossary2[key]);
+        }
+        NSLog(@"%i",max(4, 2));
         
     }
     return 0;
 }
+
+BOOL isUnique(NSString *str) {
+    if ([str length]>256) {
+        return YES;
+    }
+    NSMutableArray[
+}
+
+
 
